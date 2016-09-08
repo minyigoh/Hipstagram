c6bae9be306f9b67c149d0d6d054ccfa3617734c
Goh Min-Yi <gohmin-yi@Min-Yis-MacBook.local>
2016-09-08 11:49:35 +0800

    first commit

---
+++ Hipstagram/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hipstagram
+//
+//  Created by Goh Min-Yi on 08/09/2016.
+//  Copyright © 2016 Goh Min-Yi. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
